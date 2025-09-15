0f273fdd834685aba809b6b90b617dbe5938b30c
Anothai16 <anothai160347@gmail.com>
2025-09-15 10:39:24 +0700

    feat: Update environment configuration and replace background image in AuthLayout

--- smart-traffic-light-ui/src/components/layouts/AuthLayout/Side.tsx
+++ smart-traffic-light-ui/src/components/layouts/AuthLayout/Side.tsx
@@ -17,29 +17,11 @@ const Side = ({ children, content, ...rest }: SideProps) => {
                     backgroundImage: `url('/img/others/auth-side-bg.jpg')`,
                 }}
             >
-                {/* <Logo mode="dark" /> */}
-                <div>
-                    <p>
-                    </p>
+                <div className="text-white text-3xl font-bold">
+                    Smart Traffic Light
                 </div>
                 <div>
-                    <div className="mb-6 flex items-center gap-4">
-                        <Avatar
-                            className="border-2 border-white"
-                            shape="circle"
-                            src="/img/avatars/thumb-10.jpg"
-                        />
-                        <div className="text-white">
-                            <div className="font-semibold text-base">
-                                Brittany Hale
-                            </div>
-                            <span className="opacity-80">CTO, Onward</span>
-                        </div>
-                    </div>
-                    <p className="text-lg text-white opacity-80">
-                        Elstar comes with a complete set of UI components
-                        crafted with Tailwind CSS, it fulfilled most of the use
-                        case to create modern and beautiful UI and application
+                    <p>
                     </p>
                 </div>
                 <span className="text-white">
